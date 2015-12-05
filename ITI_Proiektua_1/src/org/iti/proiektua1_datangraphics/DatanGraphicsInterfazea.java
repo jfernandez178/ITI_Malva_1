f7024349f09c4487017af234945693443d7f7f69
aorbegozo005 <aorbegozo005@ikasle.ehu.es>
2015-12-05 11:14:48 +0100

    Pomodoro 5
    Se ha camiado totalmente el layout donde se muestra el ejemplo.
    
    Se usado un BorderLayout con dos paneles hijos. Uno contiene los botones
    siguiente y salir posicionados a su vez con otro BorderLayout. El boton
    de salir se ha puesto más pequeño que el boton de siguiente.
    
    El otro es el panel del contenido principal. Este panel se ha dividido
    en dos utilizando GridLayout obteniendo dos paneles con el mismo tamaño
    un al lado del otro. En cada uno de estos paneles se ha utilizado
    BorderLayout para poner arriba un label indicando lo que es (codigo o
    explicacion) y todo lo demas lo cubre un jtextarea que se rellenara con
    el codigo y la explicacion.

--- ITI_Proiektua_1/src/org/iti/proiektua1_datangraphics/DatanGraphicsInterfazea.java
+++ ITI_Proiektua_1/src/org/iti/proiektua1_datangraphics/DatanGraphicsInterfazea.java
@@ -4,22 +4,50 @@ import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
 
 public class DatanGraphicsInterfazea extends JFrame{
 	public DatanGraphicsInterfazea() {
+		
+		setSize(600, 600);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.NORTH);
+		getContentPane().add(panel, BorderLayout.SOUTH);
 		panel.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNewLabel = new JLabel("Enunciado:");
-		panel.add(lblNewLabel, BorderLayout.WEST);
+		JButton btnNewButton = new JButton("Salir");
+		panel.add(btnNewButton, BorderLayout.WEST);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		panel.add(lblNewLabel_1, BorderLayout.CENTER);
+		JButton btnNewButton_1 = new JButton("Siguiente");
+		panel.add(btnNewButton_1, BorderLayout.CENTER);
 		
-		setSize(600, 600);
+		JPanel panel_1 = new JPanel();
+		getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_1.add(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("C\u00F3digo:");
+		panel_2.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JTextArea textArea = new JTextArea();
+		panel_2.add(textArea, BorderLayout.CENTER);
+		
+		JPanel panel_3 = new JPanel();
+		panel_1.add(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("Explicaci\u00F3n:");
+		panel_3.add(lblNewLabel_1, BorderLayout.NORTH);
+		
+		JTextArea textArea_1 = new JTextArea();
+		panel_3.add(textArea_1, BorderLayout.CENTER);
 		
 		//TODO GEro kendu probak egiteko bakarrik
 		DatanGraphicsLogika l = new DatanGraphicsLogika();
